f26dfb3b9eae3a8140a826269b9d888bac80717b
AndyW22 <andrew.waller24@outlook.com>
2021-01-18 19:22:31 +0000

    added addItem and removeItem functionality

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,28 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
+import {
+  clearItemFromCart,
+  removeItem,
+  addItem,
+} from '../../redux/cart/cart.actions';
 import './checkout-item.styles.scss';
 
 const mapDispatchToProps = (dispatch) => ({
   clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
 });
 
 export default connect(
   null,
   mapDispatchToProps,
-)(({ cartItem, clearItem }) => {
-  const { name, imageUrl, price, quantity } = cartItem;
-  return (
-    <div className="checkout-item">
-      <div className="image-container">
-        <img src={imageUrl} alt="item" />
+)(
+  ({
+    cartItem,
+    clearItem,
+    addItem: addNewItem,
+    removeItem: removeExistingItem,
+  }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+    return (
+      <div className="checkout-item">
+        <div className="image-container">
+          <img src={imageUrl} alt="item" />
+        </div>
+        <span className="name">{name}</span>
+        <span className="quantity">
+          <div className="arrow" onClick={() => removeExistingItem(cartItem)}>
+            &#10094;
+          </div>
+          <span className="value">{quantity}</span>
+          <div className="arrow" onClick={() => addNewItem(cartItem)}>
+            &#10095;
+          </div>
+        </span>
+        <span className="price">£{price}</span>
+        <span className="remove-button" onClick={() => clearItem(cartItem)}>
+          &#10005;
+        </span>
       </div>
-      <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
-      <span className="price">{price}</span>
-      <span className="remove-button" onClick={() => clearItem(cartItem)}>
-        &#10005;
-      </span>
-    </div>
-  );
-});
+    );
+  },
+);
