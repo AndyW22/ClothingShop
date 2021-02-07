59914d1ae960162131d721d5ee7490945f29830a
AndyW22 <andrew.waller24@outlook.com>
2021-02-07 13:51:45 +0000

    Edited stripe payment message

--- client/src/components/stripe/stripe-button.component.jsx
+++ client/src/components/stripe/stripe-button.component.jsx
@@ -17,7 +17,7 @@ const StripeCheckoutButton = ({ price }) => {
       },
     })
       .then((response) => {
-        alert('succesful payment');
+        alert('Payment was successful');
       })
       .catch((error) => {
         console.log('Payment Error: ', error);
