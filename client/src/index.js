8bf45e274424b9ae434951880ce6b4891734cc7a
AndyW22 <andrew.waller24@outlook.com>
2021-02-16 20:17:51 +0000

    Added service worker

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import * as serviceWorker from './serviceworker';
+import * as serviceWorker from './serviceWorker';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 import App from './App';
